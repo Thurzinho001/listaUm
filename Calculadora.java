7bee0681ec0dc30bff3c678487ed88f7f20443d8
Thurzinho001 <168425564+Thurzinho001@users.noreply.github.com>
2024-05-15 11:08:04 -0300

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,19 @@
+public class Calculadora {
+	
+	public int soma(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtrair(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiplicar(int a, int b) {
+		return a * b;
+	}
+	
+	public int divisão(int a, int b) {
+		return a / b;
+	}
+
+}
