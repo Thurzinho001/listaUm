151bee2170729dda1ba5e43461eff41925865afb
Thurzinho001 <168425564+Thurzinho001@users.noreply.github.com>
2024-05-20 08:27:30 -0300

    Create QuestaoOnze.java

---
+++ QuestaoOnze.java
@@ -0,0 +1,35 @@
+public class QuestaoOnze{
+	private String nome;
+	private String raca;
+	private int idade;
+	
+	public QuestaoOnze(String novoNome, String novaRaca, int novaIdade) {
+		this.nome = novoNome;
+		this.raca = novaRaca;
+		this.idade = novaIdade;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public String getRaca() {
+		return raca;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+	public void setRaca(String novaRaca) {
+		this.raca = novaRaca;
+	}
+	public void setIdade(int novaIdade) {
+		this.idade = novaIdade;
+	}
+	
+	public String miar() {
+		return "Miau!";
+	}
+}
