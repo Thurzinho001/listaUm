3708f1448dd7fe6f5e70ced6b69a9132bfe4acb1
Thurzinho001 <168425564+Thurzinho001@users.noreply.github.com>
2024-05-20 08:59:27 -0300

    Create QuestaoDezenove.java

---
+++ QuestaoDezenove.java
@@ -0,0 +1,62 @@
+public class QuestaoDezenove {
+	private String rua;
+	private int numero;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String cep;
+	
+	public QuestaoDezenove (String novaRua, int novoNumero, String novoComplemento, String novoBairro, String novaCidade, String novoEstado, String novoCep) {
+		this.rua = novaRua;
+		this.numero = novoNumero;
+		this.complemento = novoComplemento;
+		this.bairro = novoBairro;
+		this.cidade = novaCidade;
+		this.estado = novoEstado;
+		this.cep = novoCep;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public String getCep() {
+		return cep;
+	}
+	
+	public void setRua(String novaRua) {
+		this.rua = novaRua;
+	}
+	public void setNumero(int novoNumero) {
+		this.numero = novoNumero;
+	}
+	public void setComplemento(String novoComplemento) {
+		this.complemento = novoComplemento;
+	}
+	public void setBairro(String novoBairro) {
+		this.bairro = novoBairro;
+	}
+	public void setCidade(String novaCidade) {
+		this.cidade = novaCidade;
+	}
+	public void setEstado(String novoEstado) {
+		this.estado = novoEstado;
+	}
+	public void setCep(String novoCep) {
+		this.cep = novoCep;
+	}
+}
